ab913f6293360bc70f709e4ab07addec2e356403
Raj Padmanabh <61701158+padmanabh007@users.noreply.github.com>
2021-06-14 15:05:17 +0530

    Add files via upload

---
+++ Gpython/osmodule.py
@@ -0,0 +1,30 @@
+import os
+
+
+#print(os.name)
+
+#print(os.path)
+
+#print(os.environ)
+
+#print(os.getcwd())
+
+#print(os.fsencode("resume"))
+
+#print(os.fsdecode("resume"))
+
+#print(os.fspath("D:\Pythons"))
+
+#print (os.getlogin())
+
+#print (os.getppid())
+
+'''
+shut=input('Do you want to shutdown (y/n) ')
+if shut=='n' :
+    exit()
+else :
+    print(os.system('shutdown /s /t 1'))
+'''
+
+print (os.CryptGenRandom())
