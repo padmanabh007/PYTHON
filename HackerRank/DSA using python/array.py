50722c063325bb628eec639b2907d5a2ff20503a
Raj Padmanabh <61701158+padmanabh007@users.noreply.github.com>
2021-06-15 21:51:44 +0530

    Update array.py

--- HackerRank/DSA using python/array.py
+++ HackerRank/DSA using python/array.py
@@ -1,8 +1,8 @@
 #Array data structure using python
 
 l=[]  #Declaring a list to store values in the array
-for i in range(int(input("Enter the number of elements required :"))):
-    l.append(int(input(f"Enter the {i+1} element :")))
+for i in range(int(input("Enter the number of elements required : "))):
+    l.append(int(input(f"Enter the {i+1} element : ")))
 
 #to print all the elements
 print(*l)
@@ -21,17 +21,23 @@ for i in range(len(l)-1):
 print(*l)
 
 #Enter the elements to be find
-find=int(input("Enter the elements to search"))
+find=int(input("Enter the elements to search : "))
 for i in l :
     if i==find:
         print(f"Element find at position {l.index(i)}")
         break
+else:
+    print("Element not found")
 
 #Enter the elements to delete
-delete=int(input("Enter the elements to delete"))
+delete=int(input("Enter the elements to delete : "))
 for i in l :
     if i==delete:
         l.pop(l.index(i))
         print('Deleted')
         break
+else:
+    print("Wrong input for delete ")
+        
+        
 print(*l)
