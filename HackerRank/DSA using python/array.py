48d06b7084ecf74c0a60adb54831b25f15e19ae5
Raj Padmanabh <61701158+padmanabh007@users.noreply.github.com>
2021-06-15 20:13:15 +0530

    Add files via upload

---
+++ HackerRank/DSA using python/array.py
@@ -0,0 +1,37 @@
+#Array data structure using python
+
+l=[]  #Declaring a list to store values in the array
+for i in range(int(input("Enter the number of elements required :"))):
+    l.append(int(input(f"Enter the {i+1} element :")))
+
+#to print all the elements
+print(*l)
+
+
+#SORTING using python inbuilt function
+l.sort()
+print(*l)
+
+#Sorting using without using inbuilt python functions
+for i in range(len(l)-1):
+    for j in range(i,len(l)-1):
+        if l[j]>l[j+1] :
+            l[j],l[j+1]=l[j+1],l[j]
+
+print(*l)
+
+#Enter the elements to be find
+find=int(input("Enter the elements to search"))
+for i in l :
+    if i==find:
+        print(f"Element find at position {l.index(i)}")
+        break
+
+#Enter the elements to delete
+delete=int(input("Enter the elements to delete"))
+for i in l :
+    if i==delete:
+        l.pop(l.index(i))
+        print('Deleted')
+        break
+print(*l)
